eb087dafb0649c87ce27efb1c060a7aa7307ba4a
ricardohitalo <ricardohitalo1@gmail.com>
2022-04-22 16:44:48 -0300

    tratando parametros da rota com expressoes regulares

--- routes/web.php
+++ routes/web.php
@@ -24,12 +24,11 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 Route::get('/contato', 'ContatoController@contato');
 
 Route::get(
-    '/contato/{nome?}/{categoria?}/{assunto?}/{mensagem?}',
+    '/contato/{nome?}/{categoria_id?}',
     function(
         string $nome = 'Desconhecido',
-        string $categoria = 'Informacao',
-        string $assunto = 'Contato',
-        string $mensagem = 'mensagem nao adicionada'
-    ) {
-    echo 'Estamos aqui: '.$nome.' - '.$categoria.' - '.$assunto.' - '.$mensagem;
-});
+        int $categoria_id = 1
+    ){
+       echo 'Estamos aqui: '.$nome.' - '.$categoria_id;
+    }
+)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
