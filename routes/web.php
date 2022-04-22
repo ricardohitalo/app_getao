c9929bff8e5c89885e17f5fde4cf591bab508384
ricardohitalo <ricardohitalo1@gmail.com>
2022-04-22 18:14:29 -0300

    agrupando rotas

--- routes/web.php
+++ routes/web.php
@@ -23,12 +23,20 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
 Route::get('/contato', 'ContatoController@contato');
 
-Route::get(
-    '/contato/{nome?}/{categoria_id?}',
-    function(
-        string $nome = 'Desconhecido',
-        int $categoria_id = 1
-    ){
-       echo 'Estamos aqui: '.$nome.' - '.$categoria_id;
-    }
-)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
+Route::get('/login', function(){ return 'Login'; });
+
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){ return 'Clientes'; });
+    Route::get('/fornecedores', function(){ return 'Fornecedores'; });
+    Route::get('/produtos', function(){ return 'Produtos'; });
+});
+
+// Route::get(
+//     '/contato/{nome?}/{categoria_id?}',
+//     function(
+//         string $nome = 'Desconhecido',
+//         int $categoria_id = 1
+//     ){
+//        echo 'Estamos aqui: '.$nome.' - '.$categoria_id;
+//     }
+// )->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
