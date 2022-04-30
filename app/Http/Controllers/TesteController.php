89b31dcf1ae1bae8a2d23bf469573a6955cddc07
ricardohitalo <ricardohitalo1@gmail.com>
2022-04-30 08:48:51 -0300

    encaminhando parametros do controlador para visualizacao

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -8,6 +8,10 @@ class TesteController extends Controller
 {
     public function teste(int $p1,int $p2) {
         // echo view('teste');
-        echo "$p1 + $p2 = ".($p1+$p2);
+        // echo "$p1 + $p2 = ".($p1+$p2);
+        // return view('site.teste', ['p1' => $p1, 'p2' => $p2]);
+        // return view('site.teste', compact('p1', 'p2'));
+
+        return view('site.teste')->with('p1', $p1)->with('p2', $p2)
     }
 }
---
+++ resources/views/site/teste.blade.php
@@ -0,0 +1,2 @@
+Teste P1 = {{ $p1 }} <br>
+Teste P1 = {{ $p2 }}
