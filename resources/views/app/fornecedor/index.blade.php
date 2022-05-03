b6e1768f3cf80c96f6b0d6f9869e1f7f4578202a
ricardohitalo <ricardohitalo1@gmail.com>
2022-05-03 15:31:08 -0300

    rota, controller e blade fonecedor

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,1 +1,23 @@
-Fornecedor
+<h4>Fornecedor</h4>
+
+@php
+    
+    // if() {
+
+    // } elseif {
+
+    // } else {
+
+    // }
+
+@endphp
+
+@dd($fornecedores)
+
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fonrnecedores cadastrados </h3>
+@elseif(count($fornecedores) > 10)
+    <h3>Existem muitos fornecedores cadastrados</h3>
+@else
+    <h3>Não exitem fornecedores cadastrados</h3>
+@endif
