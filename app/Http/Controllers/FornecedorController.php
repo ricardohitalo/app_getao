fe03c891ce6290156c845fe8d90ebc76c698ca7c
ricardohitalo <ricardohitalo1@gmail.com>
2022-05-04 08:02:23 -0300

    usando operador de valor default

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -11,7 +11,7 @@ class FornecedorController extends Controller
             0 => [
                 'nome' => 'Fornecedor 1',
                 'status' => 'N',
-                'cnpj' => ''
+                'cnpj' => '00.000.000/0000-00'
             ],
             1 => [
                 'nome' => 'Fornecedor 2',
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -18,12 +18,13 @@
     <br>
     Status: {{ $fornecedores[0]['status'] }}
     <br>
-    @isset ($fornecedores[0]['cnpj'])
+    CNPJ: {{$fornecedores[0]['cnpj'] ?? ''}}
+    {{-- @isset ($fornecedores[0]['cnpj'])
         CNPJ: {{ $fornecedores[0]['cnpj'] }}
             @empty($fornecedores[0]['cnpj'])
               - Vazio  
             @endempty
-    @endisset
+    @endisset --}}
 
     {{-- @if($fornecedores[0]['status'] == 'N')
             <h3>Fornecedor Inativo</h3>
