d5c4501044dc71008902b801643497bb594aef20
ricardohitalo <ricardohitalo1@gmail.com>
2022-05-04 07:27:06 -0300

    usando operador unless

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,4 +1,4 @@
-<h4>Fornecedor</h4>
+<h3>Fornecedor</h3>
 
 @php
     
@@ -12,12 +12,22 @@
 
 @endphp
 
-@dd($fornecedores)
+{{-- @dd($fornecedores) --}}
+Fornecedor: {{ $fornecedores[0]['nome'] }} <br>
+Status: {{ $fornecedores[0]['status'] }}
+@if($fornecedores[0]['status'] == 'N')
+        <h3>Fornecedor Inativo</h3>
+@endif
 
-@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+@unless ($fornecedores[0]['status'] == 'S')
+        Fornecedor Inativo
+    
+@endunless
+
+{{-- @if(count($fornecedores) > 0 && count($fornecedores) < 10)
     <h3>Existem alguns fonrnecedores cadastrados </h3>
 @elseif(count($fornecedores) > 10)
     <h3>Existem muitos fornecedores cadastrados</h3>
 @else
     <h3>Não exitem fornecedores cadastrados</h3>
-@endif
+@endif --}}
