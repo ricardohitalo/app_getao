78c62b8aa58ed89d1d2c1dc1331ad96f296f1d5f
ricardohitalo <ricardohitalo1@gmail.com>
2022-05-05 16:06:17 -0300

    route post contato

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'PrincipalController@principal')->name('site.index');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobre-nos');
+
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::post('/contato', 'ContatoController@contato')->name('site.contato');
+
 Route::get('/login', function(){ return 'Login'; })->name('site.login');
 
 Route::prefix('/app')->group(function(){
