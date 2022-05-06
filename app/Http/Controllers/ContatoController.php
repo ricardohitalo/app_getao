4b78f106f3ae74e7cecde008cb59c28af5e8799b
ricardohitalo <ricardohitalo1@gmail.com>
2022-05-06 11:05:16 -0300

    create model and migrate SiteContato

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class ContatoController extends Controller
 {
     public function contato() {
+        // var_dump($_POST);
         echo view('site.contato');
     }
 }
