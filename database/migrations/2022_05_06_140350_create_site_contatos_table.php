6a2bd553849934d0069b6e5bad694ae0800fa711
ricardohitalo <ricardohitalo1@gmail.com>
2022-05-09 10:45:06 -0300

    fix/attribute mensagem

--- database/migrations/2022_05_06_140350_create_site_contatos_table.php
+++ database/migrations/2022_05_06_140350_create_site_contatos_table.php
@@ -20,7 +20,7 @@ class CreateSiteContatosTable extends Migration
             $table->string('telefone', 15);
             $table->string('email', 80);
             $table->integer('motivo_contato');
-            $table->text('menssagem', );
+            $table->text('mensagem', );
         });
     }
 
