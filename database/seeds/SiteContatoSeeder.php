f058a7727cc0f5b2232bac1b7db38e10da4215ca
ricardohitalo <ricardohitalo1@gmail.com>
2022-05-20 09:49:50 -0300

    using seeder to add contato to database

---
+++ database/seeds/SiteContatoSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\SiteContato;
+use Illuminate\Database\Seeder;
+
+class SiteContatoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $contato = new SiteContato();
+        $contato->nome = 'Fulano de Tal';
+        $contato->telefone = '(99) 91234-5678';
+        $contato->email = 'flano@mail.com.br';
+        $contato->motivo_contato = 2;
+        $contato->mensagem = 'Fulano de Tal';
+        $contato->save();
+    }
+}
