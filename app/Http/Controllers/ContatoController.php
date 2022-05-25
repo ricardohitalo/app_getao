cd087888c0719d8b6734e2f4d26085f0fb146075
ricardohitalo <ricardohitalo1@gmail.com>
2022-05-25 15:38:07 -0300

    save fomr contato in DB

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\SiteContato;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato() {
+    public function contato(Request $request) {
         // var_dump($_POST);
-        echo view('site.contato');
+
+        // $contato = new SiteContato();
+        // $contato->nome = $request->input('nome');
+        // $contato->telefone = $request->input('telefone');
+        // $contato->email = $request->input('email');
+        // $contato->motivo_contato = $request->input('motivo_contato');
+        // $contato->mensagem = $request->input('mensagem');
+    
+        // // print_r($contato->getAttributes());
+        // $contato->save();
+
+        $contato = new SiteContato();
+        $contato->create($request->all());
+        // $contato->save();
+        // print_r($contato->getAttributes());
+        
+        return view('site.contato');
     }
 }
