6b1f2ae74289f9c4a4f1ee9bc89925dd13fe52b6
ricardohitalo <ricardohitalo1@gmail.com>
2022-05-25 17:08:28 -0300

    request old input

--- resources/views/site/layout/_components/form_contato.blade.php
+++ resources/views/site/layout/_components/form_contato.blade.php
@@ -1,20 +1,25 @@
 
 <form action="{{ route('site.contato') }}" method="post">
     @csrf
-    <input name="nome" type="text" placeholder="Nome" class="{{ $borderColor }}">
+    <input name="nome" value="{{ old('nome') }}" type="text" placeholder="Nome" class="{{ $borderColor }}">
     <br>
-    <input name="telefone" type="text" placeholder="Telefone" class="{{ $borderColor }}">
+    <input name="telefone" value="{{ old('telefone') }}" type="text" placeholder="Telefone" class="{{ $borderColor }}">
     <br>
-    <input name="email" type="text" placeholder="E-mail" class="{{ $borderColor }}">
+    <input name="email" value="{{ old('email') }}" type="text" placeholder="E-mail" class="{{ $borderColor }}">
     <br>
-    <select name="motivo_contato" class="{{ $borderColor }}">
+    <select name="motivo_contato" value="{{ old('motivo_contato') }}" class="{{ $borderColor }}">
         <option value="">Qual o motivo do contato?</option>
         <option value="1">Dúvida</option>
         <option value="2">Elogio</option>
         <option value="3">Reclamação</option>
     </select>
     <br>
-    <textarea class="{{ $borderColor }}">Preencha aqui a sua mensagem</textarea>
+    <textarea name="mensagem" class="{{ $borderColor }}">
+        @if (old('mensagem') != '')
+            {{ old('mensagem') }}
+        @else
+            Preencha aqui a sua mensagem
+    </textarea>
     <br>
     <button type="submit" class="{{ $borderColor }}">ENVIAR</button>
 </form>
